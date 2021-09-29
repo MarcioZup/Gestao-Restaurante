f65c1a2d11ea889472ad6cf1a1926ccc34cace6d
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-28 23:45:22 -0300

    Modelagem da Classe Ingredientes

--- src/Ingredientes.java
+++ src/Ingredientes.java
@@ -1,2 +1,23 @@
 public class Ingredientes {
+
+    private String nomeDoIngrediente;
+
+    public Ingredientes(String nomeDoIngrediente) {
+        this.nomeDoIngrediente = nomeDoIngrediente;
+    }
+
+    public String getNomeDoIngrediente() {
+        return nomeDoIngrediente;
+    }
+
+    public void setNomeDoIngrediente(String nomeDoIngrediente) {
+        this.nomeDoIngrediente = nomeDoIngrediente;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Nome do Ingrediente: " + nomeDoIngrediente);
+        return retorno.toString();
+    }
 }
